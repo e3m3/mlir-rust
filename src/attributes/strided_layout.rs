d6cc0dcb04a92af852c78487572752e7ef67d674
Giordano Salvador <73959795+e3m3@users.noreply.github.com>
2024-12-07 04:11:43 -0500

    Add semantics checks for strided layout attribute

--- src/attributes/strided_layout.rs
+++ src/attributes/strided_layout.rs
@@ -16,6 +16,7 @@ use crate::attributes;
 use crate::do_unsafe;
 use crate::exit_code;
 use crate::ir;
+use crate::types;
 
 use attributes::IRAttribute;
 use exit_code::exit;
@@ -23,6 +24,7 @@ use exit_code::ExitCode;
 use ir::Attribute;
 use ir::Context;
 use ir::TypeID;
+use types::shaped::Shaped;
 
 #[derive(Clone)]
 pub struct StridedLayout(MlirAttribute);
@@ -60,16 +62,40 @@ impl StridedLayout {
         do_unsafe!(mlirStridedLayoutAttrGetOffset(self.0))
     }
 
-    pub fn get_stride(&self, i: isize) -> i64 {
-        do_unsafe!(mlirStridedLayoutAttrGetStride(self.0, i))
+    pub fn get_stride(&self, i: isize) -> Result<i64, String> {
+        if i < 0 || i >= self.num_strides() {
+            Err("Index is out of bounds for strides of memory reference".to_string())
+        } else {
+            Ok(do_unsafe!(mlirStridedLayoutAttrGetStride(self.0, i)))
+        }
     }
 
     pub fn get_type_id() -> TypeID {
         TypeID::from(do_unsafe!(mlirStridedLayoutAttrGetTypeID()))
     }
 
+    pub fn is_dynamic_offset(&self) -> bool {
+        self.get_offset() == Shaped::dynamic_size()
+    }
+
+    pub fn is_dynamic_stride(&self, i: isize) -> Result<bool, String> {
+        if i < 0 || i >= self.num_strides() {
+            Err("Index is out of bounds for strides of memory reference".to_string())
+        } else {
+            Ok(self.get_stride(i)? == Shaped::dynamic_size())
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.num_strides() == 0
+    }
+
     pub fn num_strides(&self) -> isize {
         do_unsafe!(mlirStridedLayoutAttrGetNumStrides(self.0))
+    }
+
+    pub fn rank(&self) -> isize {
+        self.num_strides()
     }
 }
 
