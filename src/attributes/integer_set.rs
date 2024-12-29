a87a15cc99c6c1ee0f5b1d9c450f235649692ada
Giordano Salvador <73959795+e3m3@users.noreply.github.com>
2024-12-29 10:34:30 -0500

    Update integet set attribute implementation

--- src/attributes/integer_set.rs
+++ src/attributes/integer_set.rs
@@ -7,21 +7,37 @@ use mlir_sys::MlirAttribute;
 use mlir_sys::mlirIntegerSetAttrGetTypeID;
 
 use crate::attributes;
+use crate::dialects;
 use crate::do_unsafe;
 use crate::exit_code;
 use crate::ir;
 
 use attributes::IAttribute;
+use dialects::affine::Set as AffineSet;
 use exit_code::ExitCode;
 use exit_code::exit;
 use ir::Attribute;
+use ir::Context;
+use ir::StringBacked;
+use ir::StringRef;
 use ir::TypeID;
 
 #[derive(Clone)]
 pub struct IntegerSet(MlirAttribute);
 
 impl IntegerSet {
-    pub fn from(attr: MlirAttribute) -> Self {
+    pub fn new(set: AffineSet) -> Self {
+        let context = set.get_context();
+        let s = StringBacked::from(format!("affine_set<{}>", set));
+        Self::new_string(&context, &s.as_string_ref())
+    }
+
+    pub fn new_string(context: &Context, s: &StringRef) -> Self {
+        let mut attr = Attribute::from_parse(context, s);
+        Self::from_checked(*attr.get_mut())
+    }
+
+    pub fn from_checked(attr: MlirAttribute) -> Self {
         let attr_ = Attribute::from(attr);
         if !attr_.is_integer_set() {
             eprint!("Cannot coerce attribute to integer set attribute: ");
@@ -29,7 +45,7 @@ impl IntegerSet {
             eprintln!();
             exit(ExitCode::IRError);
         }
-        IntegerSet(attr)
+        Self::from(attr)
     }
 
     pub fn get(&self) -> &MlirAttribute {
@@ -42,6 +58,12 @@ impl IntegerSet {
 
     pub fn get_type_id() -> TypeID {
         TypeID::from(do_unsafe!(mlirIntegerSetAttrGetTypeID()))
+    }
+}
+
+impl From<MlirAttribute> for IntegerSet {
+    fn from(attr: MlirAttribute) -> Self {
+        Self(attr)
     }
 }
 
